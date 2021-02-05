7bb6db84dd7f3159a8c6c5ab3323743499467c52
Venkat Ragavan <mail2venkat21@gmail.com>
2021-02-05 20:58:59 +0530

    Added mergeSort

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react'
-import './Main.css';
+import {mergeSort} from './mergeSort';
 const DEFAULT_COLOR = "red";
+const SPECIFIC_COLOR = "yellow";
 export class Main extends Component {
     constructor(){
         super();
         this.state = {
             lock: false,
-            size: 200,
+            size: 100,
             array: [],
         }
     }
     initArray(){
         const array = [];
         for (let i = 0; i < this.state.size; i++) {
-        array.push(generateRandomInt(5, 730));
+        array.push(generateRandomInt(5, 800));
         }
         this.setState({array});
         console.log(100/this.state.size)
@@ -23,7 +24,7 @@ export class Main extends Component {
     }
     insertionSort(){
         this.setState({lock: !this.state.lock})
-        let arr = this.state.array;
+        let arr = this.state.array.slice();
         var n = this.state.size;
         let changes = [];
         for (let i = 1; i < n; i++) {
@@ -42,19 +43,20 @@ export class Main extends Component {
             const [pos, ele] = changes[i]
             const changedBar = arrTab[pos].style;
             setTimeout(()=>{
-                changedBar.height = `${ele/10}vh`;
-            },i*2)
+                changedBar.height = `${ele/15}vh`;
+                changedBar.backgroundColor = SPECIFIC_COLOR;
+                setTimeout(()=>{changedBar.backgroundColor = DEFAULT_COLOR;},i*0.1)
+            },i*30)
         }
-        this.setState({array: arr})
         this.setState({lock: false})
     }
     bubbleSort(){
         this.setState({lock: !this.state.lock})
-        let arr = this.state.array;
+        let arr = this.state.array.slice();
         var n = this.state.size;
         let changes = [];
-        for(var i=0;i<n;i++){
-            for(var j=0;j<n-1;j++){
+        for(let i=0;i<n;i++){
+            for(let j=0;j<n-1;j++){
                 if(arr[j+1]<arr[j]){
                     changes.push([j,arr[j],arr[j+1]]);
                     var temp = arr[j+1];
@@ -64,30 +66,57 @@ export class Main extends Component {
             }
         }
         const arrTab = document.getElementsByClassName('array-tab');
-        for(i=0;i<changes.length-1;i++){
+        for(let i=0;i<changes.length-1;i++){
             const [pos, ele1, ele2] = changes[i]
             const changedBar1 = arrTab[pos].style;
             const changedBar2 = arrTab[pos+1].style;
             setTimeout(()=>{
-                changedBar1.height = `${ele2/10}vh`;
-                changedBar2.height = `${ele1/10}vh`;
-            },i*2)
+                changedBar1.height = `${ele2/15}vh`;
+                changedBar2.height = `${ele1/15}vh`;
+                changedBar1.backgroundColor = SPECIFIC_COLOR;
+                changedBar2.backgroundColor = SPECIFIC_COLOR;
+                setTimeout(()=>{
+                    changedBar1.backgroundColor = DEFAULT_COLOR;
+                    changedBar2.backgroundColor = DEFAULT_COLOR;
+                },i*0.1)
+            },i*30)
         }
-        this.setState({array: arr})
         this.setState({lock: false})
+    }
+    mergeSort() {
+        const changes = mergeSort(this.state.array);
+        for (let i = 0; i < changes.length; i++) {
+          const arrTab = document.getElementsByClassName('array-tab');
+          const [bar1, bar2, spe, val] = changes[i]
+            const changedBar1 = arrTab[bar1].style;
+            const changedBar2 = arrTab[bar2].style;
+            setTimeout(() => {
+                const specificStyle = arrTab[spe].style;
+                specificStyle.height = `${val/15}vh`;
+                changedBar1.backgroundColor = SPECIFIC_COLOR;
+                changedBar2.backgroundColor = SPECIFIC_COLOR;
+                setTimeout(()=>{
+                    changedBar1.backgroundColor = DEFAULT_COLOR;
+                    changedBar2.backgroundColor = DEFAULT_COLOR;
+                },i*0.1)
+            }, i * 30);
+        } 
     }
     render() {
         return (
             <div className="main container">
+                <div className="row justify-content-center align-items-center">Yoo</div><br/><br/>
                 <div className="row justify-content-center align-items-center">
                     {
                         this.state.array.map((element,index) =>{
                             return(
                                 <div className="array-tab" key={index}
                                 style={{
-                                    width: `3px`,
+                                    width: `${(50)/this.state.size}%`,
                                     backgroundColor: DEFAULT_COLOR,
-                                    height: `${element/10}vh`,
+                                    height: `${element/15}vh`,
+                                    display: "inline-block",
+                                    margin: "0 1px",
                                 }}></div>
                             )
                             
@@ -96,12 +125,12 @@ export class Main extends Component {
                 </div>
                 <br />
                 <div className="row justify-content-center">
-                <button onClick={() => {if(!this.state.lock) this.initArray()}}>New Array</button>
-                <button onClick={() => {if(!this.state.lock) this.bubbleSort()}}>Bubble Sort</button>
-                <button onClick={() => {if(!this.state.lock) this.insertionSort()}}>Insertion Sort</button>
-                <button onClick={() => {if(!this.state.lock) this.initArray()}}>Merge Sort</button>
-                <button onClick={() => {if(!this.state.lock) this.initArray()}}>Quick Sort</button>
-                <button onClick={() => {if(!this.state.lock) this.initArray()}}>Heap Sort</button>
+                    <button className="btn-primary" onClick={() => {if(!this.state.lock) this.initArray()}}>New Array</button>
+                    <button className="btn-success" onClick={() => {if(!this.state.lock) this.bubbleSort()}}>Bubble Sort</button>
+                    <button className="btn-danger" onClick={() => {if(!this.state.lock) this.insertionSort()}}>Insertion Sort</button>
+                    <button className="btn-primary" onClick={() => {if(!this.state.lock) this.mergeSort()}}>Merge Sort</button>
+                    <button disabled className="btn-success" onClick={() => {if(!this.state.lock) this.initArray()}}>Quick Sort</button>
+                    <button disabled className="btn-danger" onClick={() => {if(!this.state.lock) this.initArray()}}>Heap Sort</button>
                 </div>
             </div>
         )
---
+++ src/components/mergeSort.js
@@ -0,0 +1,37 @@
+export function mergeSort(array) {
+    var arr = array.slice();
+    var changes = [];
+    mergeSortHandler(array, 0, array.length - 1, arr, changes);
+    return changes;
+}
+  
+function mergeSortHandler(array,st,en,arr,changes){
+    if (st === en) return;
+    const mid = Math.floor((st + en) / 2);
+    mergeSortHandler(arr, st, mid, array, changes);
+    mergeSortHandler(arr, mid + 1, en, array, changes);
+    sortByMerge(array, st, mid, en, arr, changes);
+}
+  
+  function sortByMerge(array, st, mid, en, arr, changes) {
+    let k = st;
+    let i = st;
+    let j = mid + 1;
+    while (i <= mid && j <= en) {
+      if (arr[i] <= arr[j]) {
+        changes.push([i, j, k, arr[i]]);
+        array[k++] = arr[i++];
+      } else {
+        changes.push([i, j, k, arr[j]]);
+        array[k++] = arr[j++];
+      }
+    }
+    while (i <= mid) {
+      changes.push([i,i,k, arr[i]]);
+      array[k++] = arr[i++];
+    }
+    while (j <= en) {
+      changes.push([i,i,k, arr[j]]);
+      array[k++] = arr[j++];
+    }
+  }
